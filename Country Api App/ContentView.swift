863d82cbee8c1dbbb15de5abf911bd6190fb3aa2
Immortal215 <sharulshah@icloud.com>
2024-09-27 09:45:26 -0500

    added api

--- Country Api App/ContentView.swift
+++ Country Api App/ContentView.swift
@@ -1,24 +1,94 @@
-//
-//  ContentView.swift
-//  Country Api App
-//
-//  Created by Sharul M. Shah on 9/27/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var countries = [Country]()
+    @State var searchText = ""
+    @State var names: [String] = []
+    @State var screenWidth = UIScreen.main.bounds.width
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            
+            // .prefix limits to 10
+            List(searchResults, id: \.self) { country in
+                VStack {
+                    //                    Text(country.name.official)
+                    //                    Text(country.name.common)
+                    NavigationLink {
+                        ScrollView {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(lineWidth: 2)
+                                
+                                Text("Official Name : \(countries[0].name.official)")
+                                    .font(.title2)
+                                    .padding()
+                            }
+                            .fixedSize()
+                            .frame(width: screenWidth/1.2)
+                        }
+                        .navigationTitle(country)
+                        
+                        
+                    } label: {
+                        Text(country)
+                    }
+                }
+                
+            }
+            .navigationTitle("Countries")
+            
         }
-        .padding()
+        .task {
+            await fetchData()
+        }
+        .searchable(text: $searchText)
+        .onChange(of: searchText) {
+            if searchText != "" {
+                searchText = "\(searchText.first!.uppercased())" + searchText.dropFirst().lowercased()
+            }
+        }
+    }
+    
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            return names
+        } else {
+            return names.filter { $0.contains(searchText) }
+        }
+    }
+    
+    func fetchData() async {
+        guard let url = URL(string: "https://restcountries.com/v3.1/all") else {
+            print("Error forming URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode([Country].self, from: data) {
+                countries = decodedResponse
+                for i in countries {
+                    names.append(i.name.common)
+                }
+            } else {
+                print("Failed to decode response")
+            }
+        } catch {
+            print("Error fetching data: \(error.localizedDescription)")
+        }
     }
 }
 
+
+struct Country: Codable {
+    var name: CountryName
+    
+    struct CountryName: Codable {
+        var common: String
+        var official: String
+    }
+}
 #Preview {
     ContentView()
 }
--- Country Api App/Country_Api_AppApp.swift
+++ Country Api App/Country_Api_AppApp.swift
@@ -1,10 +1,3 @@
-//
-//  Country_Api_AppApp.swift
-//  Country Api App
-//
-//  Created by Sharul M. Shah on 9/27/24.
-//
-
 import SwiftUI
 
 @main
