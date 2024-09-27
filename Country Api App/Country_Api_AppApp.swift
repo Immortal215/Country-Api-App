cef41747cb6a5d77ea215897c40445988203dc98
Immortal215 <sharulshah@icloud.com>
2024-09-27 08:29:22 -0500

    Initial Commit

---
+++ Country Api App/Country_Api_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Api_AppApp.swift
+//  Country Api App
+//
+//  Created by Sharul M. Shah on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Country_Api_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Country Api AppTests/Country_Api_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Api_AppTests.swift
+//  Country Api AppTests
+//
+//  Created by Sharul M. Shah on 9/27/24.
+//
+
+import Testing
+@testable import Country_Api_App
+
+struct Country_Api_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
